d9b21cf1f4cae773b525ff27f3c294783896aa8b
luciepostiaux <luciepostiaux@yahoo.fr>
2023-11-29 00:44:45 +0100

    design

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const collapse = document.getElementById("collapse-readmore");
 
 const collapseButton = document.getElementById("collapse-button");
 collapse.addEventListener("hide.bs.collapse", (event) => {
-    collapseButton.textContent = "Read more";
+  collapseButton.textContent = "Read more";
 });
 
 collapse.addEventListener("show.bs.collapse", (event) => {
-    collapseButton.textContent = "Read less";
+  collapseButton.textContent = "Read less";
 });
